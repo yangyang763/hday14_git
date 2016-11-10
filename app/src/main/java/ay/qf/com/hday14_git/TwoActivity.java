3d64b1999ba7bb81c369dade506c9587b9e962e8
samsung <yangyang763@126.com>
2016-11-10 09:52:34 +0800

    master添加twoactivity

---
+++ app/src/main/java/ay/qf/com/hday14_git/TwoActivity.java
@@ -0,0 +1,15 @@
+package ay.qf.com.hday14_git;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by samsung on 2016/11/10.
+ */
+public class TwoActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
