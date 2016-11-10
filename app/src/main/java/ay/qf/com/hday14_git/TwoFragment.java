18a8c512f3e95cb0ec7a110d978503ff9a582438
samsung <yangyang763@126.com>
2016-11-10 10:05:55 +0800

    添加TwoFragment

--- app/src/main/java/ay/qf/com/hday14_git/TwoFragment.java
+++ app/src/main/java/ay/qf/com/hday14_git/TwoFragment.java
@@ -15,6 +15,7 @@ public class TwoFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
