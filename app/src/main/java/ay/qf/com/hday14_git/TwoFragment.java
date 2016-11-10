82009f870a39959fc03751577c5b7e63789ac963
samsung <yangyang763@126.com>
2016-11-10 10:04:28 +0800

    添加TwoFragment

---
+++ app/src/main/java/ay/qf/com/hday14_git/TwoFragment.java
@@ -0,0 +1,20 @@
+package ay.qf.com.hday14_git;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by samsung on 2016/11/10.
+ */
+public class TwoFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
