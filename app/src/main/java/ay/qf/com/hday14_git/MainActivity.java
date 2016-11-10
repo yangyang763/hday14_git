0ee87f73d3ff3e5494a0f8a42d7c2c601c44da89
samsung <yangyang763@126.com>
2016-11-10 09:09:10 +0800

    添加了第二句log

--- app/src/main/java/ay/qf/com/hday14_git/MainActivity.java
+++ app/src/main/java/ay/qf/com/hday14_git/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate: 11111");
+        Log.i(TAG, "onCreate: 22222");
     }
 }
